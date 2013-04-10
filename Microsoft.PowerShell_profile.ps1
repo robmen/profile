2c0a1861069fc4ffd2e1efb2a0ec75e3dddd4a71
Rob Mensching <rob@robmensching.com>
2013-04-10 15:23:55 -0700

    Initial configuration including posh-git

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,37 @@
+$poshPath = (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent) + '\posh-git\posh-git'
+Import-Module $poshPath
+
+Set-Alias msb $env:WinDir\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+Set-Alias msbuild $env:WinDir\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+Set-Alias msbuild35 $env:WinDir\Microsoft.NET\Framework\v3.5\MSBuild.exe
+Set-Alias msbuild2 $env:WinDir\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe
+
+$identity  = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object System.Security.Principal.WindowsPrincipal($identity)
+$administrator = $principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# posh-git settings
+$GitPromptSettings.BeforeText = '['
+$GitPromptSettings.AfterText = '] '
+$GitPromptSettings.EnableWindowTitle = ''
+Enable-GitColors
+
+function prompt
+{
+    $realLASTEXITCODE = $LASTEXITCODE
+
+    # Reset color, which can be messed up by Enable-GitColors
+    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+
+    $terminator = if ($administrator) { '!' } else { '$' }
+    $Host.UI.RawUI.WindowTitle = $pwd.ProviderPath + $terminator + ' for ' + $env:UserName + '@' + $env:ComputerName
+
+    Write-Host($pwd.ProviderPath)
+    Write-VcsStatus
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return $terminator + ' '
+}
+
+Set-Location E:\src
+Clear-Host
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-profile
+Rob Mensching's personal PowerShell profile
 =======
 
-Personal PowerShell profile
+My personal profile to configure PowerShell to my liking.
