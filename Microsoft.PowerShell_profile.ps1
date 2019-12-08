eb8b9bd05e8465369bb6cc230a4fb0a89fc8e4a0
Rob Mensching <rob@robmensching.com>
2019-12-08 14:37:11 -0800

    Update to latest version

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,31 +1,53 @@
-Set-Alias msb ${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe
-Set-Alias msbuild ${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe
+Set-Alias msb "E:\apps\Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+Set-Alias msb16 "E:\apps\Visual Studio\2019\MSBuild\Current\Bin\MSBuild.exe"
+Set-Alias msbuild "E:\apps\Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+Set-Alias msbuild15 "E:\apps\Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+Set-Alias msbuild16 "E:\apps\Visual Studio\2019\MSBuild\Current\Bin\MSBuild.exe"
+Set-Alias msbuild14 ${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe
 Set-Alias msbuild12 ${env:ProgramFiles(x86)}\MSBuild\12.0\Bin\MSBuild.exe
 Set-Alias msbuild4 $env:WinDir\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 Set-Alias msbuild35 $env:WinDir\Microsoft.NET\Framework\v3.5\MSBuild.exe
 Set-Alias msbuild2 $env:WinDir\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe
 Set-Alias iise "${env:ProgramFiles(x86)}\IIS Express\iisexpress.exe"
-Set-Alias devsetup "${env:ProgramFiles(x86)}\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com"
+Set-Alias devenv "E:\apps\Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe"
+Set-Alias devsetup "${env:ProgramFiles(x86)}\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com"
 Set-Alias dev12 "${env:ProgramFiles(x86)}\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com"
 Set-Alias windbg "${env:ProgramFiles(x86)}\Windows Kits\8.1\Debuggers\x86\windbg.exe"
+Set-Alias sn "${env:ProgramFiles(x86)}\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\sn.exe"
+Set-Alias vs 'E:\apps\Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe'
 Set-Alias touch Set-File
 Set-Alias blog New-BlogEntry
 
-function msbr
-{
-    msb -v:m -p:Configuration=Release $args
+function msbr {
+    param([Parameter(ValueFromRemainingArguments=$true)][string]$arguments)
+
+    msb -v:m -p:Configuration=Release $arguments
 }
 
-function msbm
-{
-    msb -v:m -m -nr:false $args
+function msbm {
+    param([Parameter(ValueFromRemainingArguments=$true)][string]$arguments)
+
+    msb -v:m -m -nr:false $arguments
 }
 
-function msbmr
-{
-    msb -v:m -m -nr:false -p:Configuration=Release $args
+function msbmd {
+    param([Parameter(ValueFromRemainingArguments=$true)][string]$arguments)
+
+    msb -v:m -m -nr:false -p:Configuration=Debug $arguments
 }
 
+function msbmr {
+    param([Parameter(ValueFromRemainingArguments=$true)][string]$arguments)
+
+    msb -v:m -m -nr:false -p:Configuration=Release $arguments
+}
+
+function which($name)
+{
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
+
+. E:\zin\_rg.ps1
 
 # Import-Module 'C:\Program Files (x86)\Microsoft SDKs\Windows Azure\PowerShell\Azure\Azure.psd1'
 
@@ -37,27 +59,27 @@ $administrator = $principal.IsInRole([System.Security.Principal.WindowsBuiltInRo
 # posh-git settings
 Import-Module posh-git
 
-$GitPromptSettings.BeforeText = '['
-$GitPromptSettings.AfterText = '] '
-$GitPromptSettings.WorkingForegroundColor = [ConsoleColor]::Gray
-# $GitPromptSettings.UntrackedForegroundColor = [ConsoleColor]::Gray
-$GitPromptSettings.EnableWindowTitle = ''
+$GitPromptSettings.WorkingColor.ForegroundColor = [ConsoleColor]::Gray
+$GitPromptSettings.PathStatusSeparator = ''
+$GitPromptSettings.AfterStatus.Text = '] '
 
 function prompt
 {
     $realLASTEXITCODE = $LASTEXITCODE
 
     # Reset color, which can be messed up by Enable-GitColors
-    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+    #$Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
 
     $terminator = if ($administrator) { '!' } else { '$' }
     $Host.UI.RawUI.WindowTitle = $pwd.ProviderPath + $terminator + ' for ' + $env:UserName + '@' + $env:ComputerName
 
-    Write-Host($pwd.ProviderPath)
-    Write-VcsStatus
+    $prompt = ""
+    $prompt += $pwd.ProviderPath + "`n"
+    $prompt += Write-VcsStatus
+    $prompt += $terminator + ' '
 
     $global:LASTEXITCODE = $realLASTEXITCODE
-    return $terminator + ' '
+    return $prompt
 }
 
 function Add-Path {
@@ -73,6 +95,27 @@ function Add-Path {
         [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::User)
         Write-Output 'PATH updated'
     }
+}
+
+function Import-DevEnv {
+  & "${env:COMSPEC}" /s /c "`"E:\apps\Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat`" -no_logo && set" | foreach-object {
+    $name, $value = $_ -split '=', 2
+    Set-Content env:\"$name" $value
+  }
+}
+
+function Import-DevEnv15 {
+  & "${env:COMSPEC}" /s /c "`"E:\apps\Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat`" -no_logo && set" | foreach-object {
+    $name, $value = $_ -split '=', 2
+    Set-Content env:\"$name" $value
+  }
+}
+
+function Import-DevEnv16 {
+  & "${env:COMSPEC}" /s /c "`"E:\apps\Visual Studio\2019\Common7\Tools\VsDevCmd.bat`" -no_logo && set" | foreach-object {
+    $name, $value = $_ -split '=', 2
+    Set-Content env:\"$name" $value
+  }
 }
 
 function Set-File {
@@ -96,6 +139,8 @@ function New-BlogEntry {
     $date = Get-Date -format 'yyyy-MM-dd'
     $entryFile = $date + ' ' + $name + '.html.md'
 
+    Push-Location $repo
+
     code $repo (Join-Path $postsFolder $entryFile)
 
     Invoke-Item $contentFolder
@@ -104,9 +149,58 @@ function New-BlogEntry {
     Start-Process "$url"
 }
 
+function Measure-Command2 ([ScriptBlock]$Expression, [int]$Samples = 1, [Switch]$Silent, [Switch]$Long) {
+<#
+.SYNOPSIS
+  Runs the given script block and returns the execution duration.
+  Discovered on StackOverflow. http://stackoverflow.com/questions/3513650/timing-a-commands-execution-in-powershell
+
+.EXAMPLE
+  Measure-Command2 { ping -n 1 google.com }
+#>
+  $timings = @()
+  do {
+    $sw = New-Object Diagnostics.Stopwatch
+    if ($Silent) {
+      $sw.Start()
+      $null = & $Expression
+      $sw.Stop()
+      Write-Host "." -NoNewLine
+    }
+    else {
+      $sw.Start()
+      & $Expression
+      $sw.Stop()
+    }
+    $timings += $sw.Elapsed
+
+    $Samples--
+  }
+  while ($Samples -gt 0)
+
+  Write-Host
+
+  $stats = $timings | Measure-Object -Average -Minimum -Maximum -Property Ticks
+
+  # Print the full timespan if the $Long switch was given.
+  if ($Long) {
+    Write-Host "Avg: $((New-Object System.TimeSpan $stats.Average).ToString())"
+    Write-Host "Min: $((New-Object System.TimeSpan $stats.Minimum).ToString())"
+    Write-Host "Max: $((New-Object System.TimeSpan $stats.Maximum).ToString())"
+  }
+  else {
+    # Otherwise just print the milliseconds which is easier to read.
+    Write-Host "Avg: $((New-Object System.TimeSpan $stats.Average).TotalMilliseconds)ms"
+    Write-Host "Min: $((New-Object System.TimeSpan $stats.Minimum).TotalMilliseconds)ms"
+    Write-Host "Max: $((New-Object System.TimeSpan $stats.Maximum).TotalMilliseconds)ms"
+  }
+}
+
+Set-Alias time Measure-Command2
+Set-Alias timethis Measure-Command2
 
 # Required VS2012 environment variable
 #$env:VisualStudioVersion="12.0"
 
-Set-Location E:\src
+Set-Location C:\src
 #Clear-Host
